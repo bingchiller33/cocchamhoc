18174d722007ce1a25abdf4fcf2641f4fa1fc9ec
tachibanayui <quan.tachibanayui@outlook.com>
2023-05-26 00:31:42 +0700

    update dal to reflect new database changes

--- SourceCode/CocChamHoc/src/java/dal/DBContext.java
+++ SourceCode/CocChamHoc/src/java/dal/DBContext.java
@@ -3,6 +3,7 @@ package dal;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -14,9 +15,15 @@ public class DBContext {
             //Change the username password and url to connect your own database
             String username = "sa";
             String password = "sa";
-            String url = "jdbc:sqlserver://localhost:1433;databaseName=PRJ301_PROJECT";
+            String url = "jdbc:sqlserver://localhost:1433;databaseName=COC_CHAM_HOC";
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-            connection = DriverManager.getConnection(url, username, password);
+            Properties properties = new Properties();
+            properties.setProperty("user", username);
+            properties.setProperty("password", password);
+            properties.setProperty("sendTimeAsDatetime", "false"); // Set sendTimeAsDatetime to false
+            
+            connection = DriverManager.getConnection(url, properties);
+            
         } catch (ClassNotFoundException | SQLException ex) {
             Logger.getLogger(DBContext.class.getName()).log(Level.SEVERE, null, ex);
         }
