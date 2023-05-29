365397da0b7e713eeefdfb8a006c26c3d0fdf018
tachibanayui <quan.tachibanayui@outlook.com>
2023-05-29 22:28:42 +0700

    Update database

--- COCCHAMHOCDB.sql
+++ COCCHAMHOCDB.sql
@@ -157,3 +157,29 @@ CREATE TABLE Ratings
                     CourseID
                 )
 );
+
+
+Insert into Categories(CategoryDescription)
+VALUES('Programing'),('Design'),('Marketing'),('Photography & Video'),('Teaching&Academi')
+
+
+Insert into Levels(LevelDescription)
+VALUES('Beginner'),('Intermediate'),('Advanced')
+
+INSERT INTO Courses (CourseBannerImage, Title, CourseDescription, PublishDate, Lecturer, DurationInSeconds, LevelID, CategoryID)
+VALUES 
+('https://example.com/image1.jpg', N'Giới thiệu về Python', N'Khóa học này giới thiệu cách sử dụng ngôn ngữ lập trình Python.', '2023-01-01', N'Nguyễn Văn A', 18000, 1, 1),
+('https://example.com/image2.jpg', N'Trí tuệ nhân tạo cho người mới bắt đầu', N'Khóa học cung cấp kiến thức cơ bản về trí tuệ nhân tạo.', '2023-02-01', N'Trần Thị B', 25000, 1, 1),
+('https://example.com/image3.jpg', N'Phát triển ứng dụng di động với React Native', N'Học cách xây dựng ứng dụng di động đa nền tảng với React Native.', '2023-03-01', N'Lê Văn C', 30000, 2, 1),
+('https://example.com/image4.jpg', N'Giới thiệu về Blockchain', N'Tìm hiểu về công nghệ Blockchain và các ứng dụng của nó.', '2023-04-01', N'Phạm Thị D', 20000, 1, 1),
+('https://example.com/image5.jpg', N'Lập trình Web với JavaScript, HTML và CSS', N'Khóa học này giúp bạn nắm vững kiến thức cơ bản về lập trình web.', '2023-05-01', N'Đặng Văn E', 27000, 1, 1),
+('https://example.com/image6.jpg', N'Machine Learning cơ bản', N'Khóa học giúp bạn tìm hiểu về machine learning và các thuật toán cơ bản.', '2023-06-01', N'Huỳnh Văn F', 32000, 2, 1),
+('https://example.com/image7.jpg', N'Phát triển ứng dụng Web với Django', N'Học cách xây dựng ứng dụng web mạnh mẽ với Django.', '2023-07-01', N'Ngô Thị G', 35000, 2, 1),
+('https://example.com/image8.jpg', N'Thực hành DevOps', N'Khóa học giới thiệu về DevOps và các công cụ liên quan.', '2023-08-01', N'Trương Văn H', 28000, 3, 1),
+('https://example.com/image9.jpg', N'Tối ưu hóa SQL', N'Tìm hiểu về cách tối ưu hóa truy vấn SQL và cải thiện hiệu suất.', '2023-09-01', N'Lý Thị I', 22000, 3, 1),
+('https://example.com/image10.jpg', N'Lập trình game với Unity', N'Khóa học này giúp bạn nắm vững kiến thức cơ bản về lập trình game với Unity.', '2023-10-01', N'Phan Văn J', 40000, 1, 1),
+('https://example.com/image11.jpg', N'UI/UX Design cơ bản', N'Khóa học giúp bạn tìm hiểu về thiết kế giao diện và trải nghiệm người dùng.', '2023-11-01', N'Mai Văn K', 24000, 1, 2),
+('https://example.com/image12.jpg', N'Thiết kế đồ họa với Adobe Photoshop', N'Học cách sử dụng Adobe Photoshop để tạo ra các sản phẩm đồ họa chuyên nghiệp.', '2023-12-01', N'Bùi Thị L', 30000, 1, 2),
+('https://example.com/image13.jpg', N'Thiết kế sản phẩm với Sketch', N'Khóa học giới thiệu về Sketch và cách sử dụng công cụ này để thiết kế sản phẩm.', '2024-01-01', N'Vũ Văn M', 27000, 2, 2),
+('https://example.com/image14.jpg', N'Thiết kế Web với Figma', N'Tìm hiểu về cách thiết kế giao diện web chuyên nghiệp với Figma.', '2024-02-01', N'Phạm Thị N', 26000, 2, 2),
+('https://example.com/image15.jpg', N'Thiết kế đồ họa với Adobe Illustrator', N'Khóa học này giúp bạn nắm vững kiến thức cơ bản về thiết kế đồ họa với Adobe Illustrator.', '2024-03-01', N'Đặng Văn O', 32000, 1, 2);
--- SourceCode/CocChamHoc/src/java/dal/DBContext.java
+++ SourceCode/CocChamHoc/src/java/dal/DBContext.java
@@ -13,9 +13,12 @@ public class DBContext {
     {
         try {
             //Change the username password and url to connect your own database
-            String username = "sa";
-            String password = "sa";
-            String url = "jdbc:sqlserver://localhost:1433;databaseName=COC_CHAM_HOC";
+            // We get these information from environment variable so that it 
+            // can be changed without recompiling our code
+            String username = System.getenv("CSUSERNAME");
+            String password = System.getenv("CSPASSWORD");
+            String url = System.getenv("CS");
+            
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
             Properties properties = new Properties();
             properties.setProperty("user", username);
